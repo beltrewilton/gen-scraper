6426e01b6e73371849472f75780d09951eb0a519
beltrewilton <beltre.wilton@gmail.com>
2022-11-04 20:29:32 -0400

    init commit :)

---
+++ genscraper.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import time
+import logging
+import traceback
+
+URL_FILENAME = 'urls.txt'
+MAX_WAIT = 10
+MAX_RETRY = 10
+
+
+class GenScraper:
+    def __init__(self, driver_name, debug=False):
+        self.debug = debug
+        self.driver_name = driver_name
+        self.driver = self.__get_driver()
+        self.logger = self.__get_logger()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, tb):
+        if exc_type is not None:
+            traceback.print_exception(exc_type, exc_value, tb)
+
+        self.driver.close()
+        self.driver.quit()
+
+        return True
+
+    def publist(self, url):
+        publist = []
+        try:
+            self.driver.get(url)
+            time.sleep(np.random.uniform(3, 4))
+            ahrefs = self.driver.find_elements(By.CSS_SELECTOR, "div.thumb-title")
+            for refs in ahrefs:
+                publist.append(refs.find_element(By.TAG_NAME, 'a').get_attribute('href'))
+        except Exception as ex:
+            print(ex)
+
+        return publist
+
+    def work(self):
+        url = 'https://dominicanasolidaria.org/directorio/vacantes/'
+        publist = self.publist(url)
+        for pub in publist:
+            self.driver.get(pub)
+            time.sleep(np.random.uniform(3, 4))
+            data_groups = self.driver.find_elements(By.CSS_SELECTOR, "div.data-group")
+            title = self.driver.find_element(By.CSS_SELECTOR, "div.col-xs-12")
+            print(title.text)
+            for data in data_groups:
+                print(data.text.replace('\n', ' '))
+            print(' . . . . . . . . . . . . . . . . . . . .\n')
+
+
+
+    def __get_logger(self):
+        # create logger
+        logger = logging.getLogger('tripadvisor-scraper')
+        logger.setLevel(logging.DEBUG)
+
+        # create console handler and set level to debug
+        fh = logging.FileHandler('ta-scraper.log')
+        fh.setLevel(logging.DEBUG)
+
+        # create formatter
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+        # add formatter to ch
+        fh.setFormatter(formatter)
+
+        # add ch to logger
+        logger.addHandler(fh)
+
+        return logger
+
+    def __get_driver(self):
+        options = Options()
+        if not self.debug:
+            options.add_argument("--headless")
+        options.add_argument("--window-size=1366,768")
+        options.add_argument("--disable-notifications")
+        options.add_experimental_option('prefs', {'intl.accept_languages': 'en_GB'})
+        input_driver = webdriver.Chrome(executable_path='./bin/{}'.format(self.driver_name), chrome_options=options)
+
+        return input_driver
---
+++ scraper.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+from genscraper import GenScraper
+import argparse
+import csv
+
+
+HEADER = ['hotel_name', 'location', 'wrote', 'rating', 'title', 'comment']
+
+
+def csv_writer(path='data/', outfile='florianopolis_data'):
+    targetfile = open(path + outfile + '.csv', mode='w', encoding='utf-8', newline='\n')
+    writer = csv.writer(targetfile, quoting=csv.QUOTE_MINIMAL)
+    writer.writerow(HEADER)
+
+    return writer
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Tripadvisor scraper.')
+    parser.add_argument('--i', type=str, default='urls.txt', help='target URLs file')
+    parser.add_argument('--numhotel', type=int, default=5, help='the max nums of hotel to being scraped.')
+    parser.add_argument('--city', type=str, default='Florianopolis', help='city name')
+    parser.add_argument('--maxcommentsperpage', type=int, default=5, help='the max nums of comment per page')
+    parser.add_argument('--lang', type=str, default='Portuguese', help='The lang of comment to being scraped.')
+    parser.add_argument('--outfile', type=str, default='dummy_data', help='Out data file name')
+    parser.add_argument('--verbose', type=int, default=0, help='Print the data in the terminal (0 = no print, 1 = print).')
+    parser.add_argument('--debug', dest='debug', action='store_true', help='Debug mode: chromedriver with graphical part')
+    parser.set_defaults(place=False, debug=False)
+
+    args = parser.parse_args()
+
+    # store reviews in CSV file
+    writer = csv_writer(outfile=args.outfile)
+
+    with GenScraper(driver_name='chromedriver', debug=args.debug) as scraper:
+        with open(args.i, 'r') as url_file:
+             for url in url_file:
+                 scraper.work()
